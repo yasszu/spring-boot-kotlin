b5956719a7898fa8b1576fbeb30d229a433bd06d
Yasuhiro Suzuki <yasszu@yahoo.com>
2019-09-28 22:57:58 +0900

    Use kotlin's router function DSL

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -3,10 +3,31 @@ package com.example.blog
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.router
 
 @SpringBootApplication
 @EnableConfigurationProperties(BlogProperties::class)
-class BlogApplication
+class BlogApplication {
+
+    @Bean
+    fun routes(userRepository: UserRepository, articleRepository: ArticleRepository) = router {
+        "/api/user".nest {
+            GET("/") { ServerResponse.ok().body(userRepository.findAll()) }
+            GET("/{login}") { ServerResponse.ok().body(userRepository.findByLogin(it.pathVariable("login"))) }
+        }
+        "/api/article".nest {
+            GET("/") { ServerResponse.ok().body(articleRepository.findAll()) }
+            GET("/{slug}") {
+                ServerResponse.ok().body(articleRepository.findBySlug(it.pathVariable("slug"))
+                        ?: throw IllegalArgumentException("Wrong article slug provided"))
+            }
+        }
+    }
+
+}
+
 
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args)
--- src/main/kotlin/com/example/blog/BlogConfiguration.kt
+++ src/main/kotlin/com/example/blog/BlogConfiguration.kt
@@ -12,12 +12,14 @@ class BlogConfiguration {
                             articleRepository: ArticleRepository) = ApplicationRunner {
 
         val smaldini = userRepository.save(User("smaldini", "Stéphane", "Maldini"))
+
         articleRepository.save(Article(
                 title = "Reactor Bismuth is out",
                 headline = "Lorem ipsum",
                 content = "dolor sit amet",
                 author = smaldini
         ))
+
         articleRepository.save(Article(
                 title = "Reactor Aluminium has landed",
                 headline = "Lorem ipsum",
