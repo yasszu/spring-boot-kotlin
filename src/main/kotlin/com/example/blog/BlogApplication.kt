0ad83e1639ddf52568dafda79a5f1134975fa8d6
Yasuhiro Suzuki <yasszu@yahoo.com>
2019-09-29 16:45:20 +0900

    create handler

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -3,32 +3,54 @@ package com.example.blog
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.context.support.beans
+import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.router
 
 @SpringBootApplication
 @EnableConfigurationProperties(BlogProperties::class)
-class BlogApplication {
+class BlogApplication
 
-    @Bean
-    fun routes(userRepository: UserRepository, articleRepository: ArticleRepository) = router {
-        "/api/user".nest {
-            GET("/") { ServerResponse.ok().body(userRepository.findAll()) }
-            GET("/{login}") { ServerResponse.ok().body(userRepository.findByLogin(it.pathVariable("login"))) }
-        }
-        "/api/article".nest {
-            GET("/") { ServerResponse.ok().body(articleRepository.findAll()) }
-            GET("/{slug}") {
-                ServerResponse.ok().body(articleRepository.findBySlug(it.pathVariable("slug"))
-                        ?: throw IllegalArgumentException("Wrong article slug provided"))
+fun main(args: Array<String>) {
+    runApplication<BlogApplication>(*args) {
+        addInitializers(beans())
+    }
+}
+
+fun beans() = beans {
+    bean<UserHandler>()
+    bean<ArticleHandler>()
+    bean {
+        router {
+            "/api/user".nest {
+                val handler = ref<UserHandler>()
+                GET("/", handler::findAll)
+                GET("/{login}", handler::find)
+            }
+            "/api/article".nest {
+                val handler = ref<ArticleHandler>()
+                GET("/", handler::findAll)
+                GET("/{slug}", handler::find)
             }
         }
     }
+}
+
+class UserHandler(private val repository: UserRepository) {
+
+    fun findAll(req: ServerRequest) = ServerResponse.ok().body(repository.findAll())
+
+    fun find(req: ServerRequest) = ServerResponse.ok().body(repository.findByLogin(req.pathVariable("login")))
 
 }
 
+class ArticleHandler(private val repository: ArticleRepository) {
 
-fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args)
+    fun findAll(req: ServerRequest) = ServerResponse.ok().body(repository.findAll())
+
+    fun find(req: ServerRequest) = ServerResponse.ok()
+            .body(repository.findBySlug(req.pathVariable("slug"))
+                    ?: throw IllegalArgumentException("Wrong article slug provided"))
+
 }
